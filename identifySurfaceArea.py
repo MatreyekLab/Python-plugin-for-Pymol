1a5805183015e3f1770a000a6aeb4cd087d81fc8
Anh Pham <tienanhsng@gmail.com>
2021-03-20 22:35:39 -0400

    commit

--- identifySurfaceArea.py
+++ identifySurfaceArea.py
@@ -13,7 +13,7 @@ def identifySA(pdb, chain, start, end):
     end = int(end)
 
     # Fetch the protein
-    cmd.fetch("pdb")
+    cmd.fetch(pdb)
 
     # Create selection and object
     cmd.select("sel_nanobody", pdb + " and " + chain)
